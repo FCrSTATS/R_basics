9be27b00dbae2031d5556cb4129b47bb0128e42b
FC_rSTATS <35891964+FCrSTATS@users.noreply.github.com>
2018-11-09 00:46:49 +0100

    Update nick_1v1.r

--- nick_1v1.r
+++ nick_1v1.r
@@ -1,4 +1,4 @@
-## 1v1 session for Nick Ungson - @NickUngson
+## 1v1 session for Nick 
 ## K-Means for Player Profiles 
 
 # load libraries 
@@ -47,6 +47,88 @@ events <-
 player.summaries <- 
   events %>% 
   filter(total.minutes.played > 270 & position.name != "Goalkeeper" & competition_id == 43) %>% 
+  group_by(player.name, type.name) %>%
+  summarise(Total = (n() / max(total.minutes.played) * 90), 
+            total.minutes.played = max(total.minutes.played)) %>% 
+  group_by(player.name) %>% 
+  spread(type.name, Total)
+
+# replace NAs with zero
+player.summaries[is.na(player.summaries)] <- 0
+
+# see all varaibles  
+allVars <- colnames(player.summaries)
+
+# keep only columns we want 
+data.to.cluster <- 
+  player.summaries %>% 
+  rename(Ball.Recovery = `Ball Recovery`) %>%
+  select(player.name, total.minutes.played, Pass.into.Box,
+         Ball.Recovery, Clearance, Dribble, Long.Pass, High.Pressure,
+         Interception, Pass, Shot.outside.box, Shot.in.box, Pressure, 
+         )
+
+# normalise the data - define function and apply to all columns that need it
+NCOL(data.to.cluster)
+normalise <- function(x) {return ( (x - min(x)) / (max(x) - min(x))) }
+normalised.data <- normalise(data.to.cluster[3:ncol(data.to.cluster)])
+
+## ----- CLUSTERS ---------------------------------
+
+# run k-means to show output
+kmeans(normalised.data, 10, nstart=50,iter.max = 15)
+
+# A key decision we need to make is how many clusters, there are a 
+# few techniques for this. 1st the Elbow 
+
+#Elbow Method for finding the optimal number of clusters
+set.seed(123)
+# Compute and plot wss for k = 2 to k = 15.
+k.max <- 15
+wss <- sapply(1:k.max, 
+              function(k){kmeans(normalised.data, k, nstart=50,iter.max = 15 )$tot.withinss})
+wss
+plot(1:k.max, wss,
+     type="b", pch = 19, frame = FALSE, 
+     xlab="Number of clusters K",
+     ylab="Total within-clusters sum of squares")
+
+# 2nd is from a bayesian perspective. Bayesian Information Criterion for 
+# expectation-maximization, initialized by hierarchical clustering for 
+# parameterized Gaussian mixture models
+
+#install.packages("mclust")
+require(mclust)
+
+d_clust <- Mclust(as.matrix(normalised.data), G=1:15, 
+                  modelNames = mclust.options("emModelNames"))
+d_clust$BIC
+plot(d_clust)
+
+## 3rds is using NbClust to calculate test 30 combinations of setups
+# returning the majorties optimal number 
+
+#install.packages("NbClust",dependencies = TRUE)
+library(NbClust)
+nb <- NbClust(normalised.data, diss=NULL, distance = "euclidean", 
+              min.nc=2, max.nc=5, method = "kmeans", 
+              index = "all", alphaBeale = 0.1)
+hist(nb$Best.nc[1,], breaks = max(na.omit(nb$Best.nc[1,])))
+
+## what should we choose!! 
+no.clusters <- 7
+
+## run the actual analysis to use 
+cluster.results <- kmeans(normalised.data, no.clusters, nstart=50,iter.max = 15)
+cluster.results$cluster
+
+table(cluster.results$cluster)
+
+cluster.results$centers
+
+## bind the results 
+data.to.cluster$Cluster <- cluster.results$cluster
+ 
   group_by(player.name, type.name) %>%
   summarise(Total = (n() / max(total.minutes.played) * 90), 
             total.minutes.played = max(total.minutes.played)) %>% 
